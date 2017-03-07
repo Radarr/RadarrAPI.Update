525692238df72a41cc20151104392fe8898635f2
AeonLucid <aeonlucid@outlook.com>
2017-03-07 08:38:39 +0100

    Added ping controller.

---
+++ RadarrAPI/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadarrAPI.Controllers
+{
+    [Route("v1/[controller]")]
+    public class PingController
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
