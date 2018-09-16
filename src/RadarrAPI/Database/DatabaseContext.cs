7079e8a6554f2fc3d99f50c69f87318b7cd32871
Mike <aeonlucid@outlook.com>
2018-09-17 01:12:18 +0200

    Improve database structure.

--- src/RadarrAPI/Database/DatabaseContext.cs
+++ src/RadarrAPI/Database/DatabaseContext.cs
@@ -21,8 +21,13 @@ namespace RadarrAPI.Database
             {
                 builder.HasKey(k => k.UpdateEntityId);
 
-                builder.Property(entity => entity.Version).IsRequired();
                 builder.HasIndex(i => new {i.Branch, i.Version}).IsUnique();
+
+                builder.Property(x => x.Version).IsRequired().HasMaxLength(32);
+                builder.Property(x => x.ReleaseDate).IsRequired();
+                builder.Property(x => x.Branch).IsRequired().HasColumnType("tinyint");
+                builder.Property(x => x.NewStr).IsRequired().HasMaxLength(8192);
+                builder.Property(x => x.FixedStr).IsRequired().HasMaxLength(8192);
 
                 builder.HasMany(u => u.UpdateFiles)
                     .WithOne(u => u.Update)
@@ -34,12 +39,21 @@ namespace RadarrAPI.Database
             modelBuilder.Entity<UpdateFileEntity>(builder =>
             {
                 builder.HasKey(k => new {k.UpdateEntityId, k.OperatingSystem});
+
+                builder.Property(x => x.OperatingSystem).IsRequired().HasColumnType("tinyint");
+                builder.Property(x => x.Filename).IsRequired().HasMaxLength(128);
+                builder.Property(x => x.Url).IsRequired().HasMaxLength(255);
+                builder.Property(x => x.Hash).IsRequired().HasMaxLength(64);
             });
 
             modelBuilder.Entity<TraktEntity>(builder =>
             {
                 builder.HasKey(k => k.Id);
                 builder.HasIndex(k => k.State);
+
+                builder.Property(x => x.State).IsRequired();
+                builder.Property(x => x.Target).IsRequired().HasMaxLength(255);
+                builder.Property(x => x.CreatedAt).IsRequired();
             });
         }
     }
---
+++ src/RadarrAPI/Database/Migrations/20180916230739_Initial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RadarrAPI.Database.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trakt",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    State = table.Column<Guid>(nullable: false),
+                    Target = table.Column<string>(maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trakt", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Updates",
+                columns: table => new
+                {
+                    UpdateEntityId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Version = table.Column<string>(maxLength: 32, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Branch = table.Column<sbyte>(type: "tinyint", nullable: false),
+                    New = table.Column<string>(maxLength: 8192, nullable: false),
+                    Fixed = table.Column<string>(maxLength: 8192, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Updates", x => x.UpdateEntityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UpdateFiles",
+                columns: table => new
+                {
+                    UpdateEntityId = table.Column<int>(nullable: false),
+                    OperatingSystem = table.Column<sbyte>(type: "tinyint", nullable: false),
+                    Filename = table.Column<string>(maxLength: 128, nullable: false),
+                    Url = table.Column<string>(maxLength: 255, nullable: false),
+                    Hash = table.Column<string>(maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UpdateFiles", x => new { x.UpdateEntityId, x.OperatingSystem });
+                    table.ForeignKey(
+                        name: "FK_UpdateFiles_Updates_UpdateEntityId",
+                        column: x => x.UpdateEntityId,
+                        principalTable: "Updates",
+                        principalColumn: "UpdateEntityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trakt_State",
+                table: "Trakt",
+                column: "State");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Updates_Branch_Version",
+                table: "Updates",
+                columns: new[] { "Branch", "Version" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trakt");
+
+            migrationBuilder.DropTable(
+                name: "UpdateFiles");
+
+            migrationBuilder.DropTable(
+                name: "Updates");
+        }
+    }
+}
