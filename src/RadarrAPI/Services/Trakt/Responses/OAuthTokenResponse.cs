3092a4e02bd52e58c235a38dbf02ae60bcf7c654
AeonLucid <aeonlucid@gmail.com>
2019-09-27 19:34:53 +0200

    Fix for real

--- src/RadarrAPI/Services/Trakt/Responses/OAuthTokenResponse.cs
+++ src/RadarrAPI/Services/Trakt/Responses/OAuthTokenResponse.cs
@@ -1,12 +1,25 @@
+using Newtonsoft.Json;
+
 namespace RadarrAPI.Services.Trakt.Responses
 {
     public class OAuthTokenResponse
     {
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
+        
+        [JsonProperty("token_type")]
         public string TokenType { get; set; }
+        
+        [JsonProperty("expires_in")]
         public int ExpiresIn { get; set; }
+        
+        [JsonProperty("refresh_token")]
         public string RefreshToken { get; set; }
+        
+        [JsonProperty("scope")]
         public string Scope { get; set; }
+        
+        [JsonProperty("created_at")]
         public long CreatedAt { get; set; }
     }
 }
